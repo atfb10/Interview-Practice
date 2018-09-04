da2ccc2b9b510c6ba910384bdf242d936f6dc3a1
Adam Forestier <aforestier10@gmail.com>
2018-09-04 15:59:35 -0500

    Add a problem statement \to unique substring problem

--- robinhood/UniqueSubstrings_In_String/UniqueSubstrings_In_String/Program.cs
+++ robinhood/UniqueSubstrings_In_String/UniqueSubstrings_In_String/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// Print all unique substrings for any given string
+/// </summary>
+
 namespace UniqueSubstrings_In_String
 {
     class Program
