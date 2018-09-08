b532e6321e1cfba03e540fa99127b85c7afc093c
Adam Forestier <aforestier10@gmail.com>
2018-09-08 14:02:38 -0500

    Compress a string

---
+++ microsoft/BinTree_To_LinkedList/BinTree_To_LinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+// convert a binary tree to a doubly linked list
+
+namespace BinTree_To_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    class BinaryTree
+    {
+        Node root;
+
+        Node head;
+
+        public void BinTreeToLinkedList(Node root)
+        {
+            // check if the root of tree is empty
+            if (root == null)
+                return;
+
+            // recursively convert right side of tree
+            BinTreeToLinkedList(root.right);
+
+            // insert the root into doubly linked list
+            root.right = head;
+
+            // Change the left pointer of previous head
+            if (head != null)
+                (head).left = root;
+
+            // convert left side of tree
+            BinTreeToLinkedList(root.left);
+        }
+
+        // print new linked list
+        public void PrintList(Node head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.data + " ");
+                head = head.right;
+            }
+        }
+    }
+}
---
+++ microsoft/StringCompression/StringCompression/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// Given an array of characters, compress the array in such
+/// a way that [aBBBAAaaa] becomes [a1B3A2a3]
+/// </summary>
+
+namespace StringCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test input
+            string test = "aBBBAAaaa";
+
+            // call method, pass in arg
+            Console.WriteLine(Compress(test));
+
+            // keep console open
+            Console.Read();
+        }
+
+       private static string Compress(string str)
+        {
+            // create stringbuilder to hold new string and counter to hold count of dups
+            StringBuilder compressed = new StringBuilder();
+            int counter = 0;
+
+            // make sure string has a length
+            if (str.Length == 0)
+            {
+                return "Please enter string with a valid length";
+            }
+
+            // loop through string
+            for (int i = 0; i < str.Length; i++)
+            {
+                // increment counter
+                counter++;
+
+                // check if strings end or next letter is not duplicate
+                if (i + 1 >= str.Length || str[i] != str[i + 1])
+                {
+                    // append character and string. Reset counter
+                    compressed.Append(str[i]);
+                    compressed.Append(counter);
+                    counter = 0;
+                }
+            }
+
+            // return the new compressed string
+            return compressed.ToString();
+        }
+    }
+}
