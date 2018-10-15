14a63f5807ad1a90343373fd7a0cf11c7177db80
Adam Forestier <aforestier10@gmail.com>
2018-10-14 20:32:56 -0500

    Return average of two given numbers

---
+++ AT&T/CalcAvg/CalcAvg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// calc average of two numbers
+
+namespace CalcAvg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test nums
+            int x = 24, y = 45;
+
+            // call method and print result
+            Console.WriteLine("{0} and {1}'s average = {2}", x, y, CalcAvg(x, y));
+
+            // keep console open
+            Console.Read();
+        }
+
+        private static float CalcAvg(int x, int y)
+        {
+            float avg;
+            int sum = x + y;
+
+            // divide sum by total numbers
+            avg = sum / (float)2;
+
+            // return avg
+            return avg;
+        }
+    }
+}
